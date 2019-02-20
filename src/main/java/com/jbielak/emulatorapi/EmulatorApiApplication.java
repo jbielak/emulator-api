9ec6fb46111d659bd4b6bcb10ed756dc0e92d52b
jbielak <justyna_bielak@outlook.com>
2019-02-20 23:25:25 +0100

    Initial commit

---
+++ src/main/java/com/jbielak/emulatorapi/EmulatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.jbielak.emulatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmulatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmulatorApiApplication.class, args);
+    }
+
+}
